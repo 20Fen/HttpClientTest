b6fa97641d9ae19b26e3d565837e04c6732db82c
Ykw <y15622969639@163.com>
2020-04-10 10:47:28 +0800

    xiaodemo

--- src/main/java/com/example/demo/controller/AuthServiceUserController.java
+++ src/main/java/com/example/demo/controller/AuthServiceUserController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.httpClientUser.AuthServiceUser;
 import com.example.demo.model.po.TestPo;
+import org.apache.http.client.utils.URIBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
@@ -10,8 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Description: controller 类
@@ -51,5 +56,27 @@ public class AuthServiceUserController {
             throw new Exception("添加失败");
         }
         return addTestPo;
+    }
+
+    public static void main(String[] args) {
+
+        String url= "www.baidu.com";
+        Map<String,String> map=new HashMap<>();
+        map.put("123","123");
+        map.put("234","23");
+        map.put("345","3");
+
+        try {
+            URIBuilder builder1 = new URIBuilder(url);
+
+            for (String o : map.keySet()) {
+                builder1.setParameter(o, map.get(o));
+            }
+            URI build = builder1.build();
+            System.out.println(build);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
     }
 }
